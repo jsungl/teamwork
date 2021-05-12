79e71f1ed43bfc11ccca7e56e18bfefbb95805e0
YoonKisang <80908532+YoonKisang@users.noreply.github.com>
2021-05-12 20:14:22 +0900

    메소드 새로 추가

--- src/com/kh/teamwork/KisangClass.java
+++ src/com/kh/teamwork/KisangClass.java
@@ -1,7 +1,12 @@
 package com.kh.teamwork;
 
 public class KisangClass {
-	public void kisang() {
+	
+	public void foo() {
+		System.out.println("foooooooooooooooooo");
+	}
+	
+	public void hi() {
 		System.out.println("asdf");
 	}
 }
